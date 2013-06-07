7689a7b71c96331c9c240baa4e5c06d25a950e07
Jose Ramón Álvarez Padial <joseavi93@gmail.com>
2013-06-07 14:05:59 +0200

    Cambios PacMan

--- Comecocos/src/PacMan.java
+++ Comecocos/src/PacMan.java
@@ -7,12 +7,10 @@ import javax.imageio.ImageIO;
 
 public class PacMan extends Personaje{
 	
-	private BufferedImage img;
-	private Point2D.Double ds;	// desplazamiento (x, y) para seleccionar el sprite del
-	                            // pacman que se tiene que diburar en un instante determinado.
+	private BufferedImage img;	// pacman que se tiene que diburar en un instante determinado.
 	
 	public PacMan(Laberinto l, String fichero) {
-		super(NombresPersonaje.PACMAN, l);
+		super(NombresPersonaje.PACMAN, l, true, 6, 5);
 		try {
 			img = ImageIO.read(PacMan.class.getResource(fichero));
 		} catch (IOException e) {}
@@ -22,40 +20,11 @@ public class PacMan extends Personaje{
 		return img;
 	}
 	
-	public Point2D.Double getCoordenadasSprite() {
-		
-		return null;
-	}
-	
 	
 	public void mover(double velocidad, Direccion nuevaDireccion){
 		// El parámetro nuevaDirección representa la dirección que tiene que tomar si fuese posible.
 		super.mover(velocidad);
-		Direccion d = getDireccion();
-		Point2D.Double p = getPosicion();
-		if(d != nuevaDireccion) 
-			switch (d){
-		case ARRIBA:
-			if ((nuevaDireccion == Direccion.ABAJO) || ((int) p.y % 19 == 0 && !muro(nuevaDireccion))){
-				setDireccion(nuevaDireccion);
-			}
-			break;
-		case IZDA:
-			if ((nuevaDireccion == Direccion.DERECHA) || ((int) p.x % 19 == 0 &&!muro(nuevaDireccion))){
-				setDireccion(nuevaDireccion);
-			}
-			break;
-		case ABAJO:
-			if ((nuevaDireccion == Direccion.ARRIBA) || ((int) p.y % 19 == 0 &&!muro(nuevaDireccion))){
-				setDireccion(nuevaDireccion);
-			}
-			break;
-		case DERECHA:
-			if ((nuevaDireccion == Direccion.IZDA) || ((int) p.x % 19 == 0 &&!muro(nuevaDireccion))){
-				setDireccion(nuevaDireccion);
-			}
-			
-			break;
-		}
+		if (getDireccion() != nuevaDireccion && puedeCambiarDireccion(nuevaDireccion))
+			setDireccion(nuevaDireccion);
 	}
 }
